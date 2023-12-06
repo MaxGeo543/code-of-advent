f962ac4a36dd715cae8dd373c41bdb9d0009ee66
MaxGeo <maximilian050403g@web.de>
2023-12-06 21:27:37 +0100

    changed folder structure slightly and changed template.py

--- template.py
+++ template.py
@@ -1,24 +1,24 @@
 '''
+puzzle description
 '''
 
+# get paths of input files
 from pathlib import Path
 mod_path = Path(__file__).parent
 PUZZLE_INPUT_PATH = Path(mod_path, "puzzle_input.txt")
 TEST_INPUT_PATH = Path(mod_path, "test_input.txt")
 
+
 def part1(input_file):
     # load the puzzle input into a variable
     with open(input_file, "r") as file:
         puzzle_input = file.readlines()
+    
+    # print(result)
+    # return result
 
-def part2(input_file):
-    # load the puzzle input into a variable
-    with open(input_file, "r") as file:
-        puzzle_input = file.readlines()
 
 if __name__ == "__main__":
     # assert part1(TEST_INPUT_PATH)
-    # assert part2(TEST_INPUT_PATH)
     
-    part1(PUZZLE_INPUT_PATH)
-    part2(PUZZLE_INPUT_PATH)
+    # part1(PUZZLE_INPUT_PATH)
